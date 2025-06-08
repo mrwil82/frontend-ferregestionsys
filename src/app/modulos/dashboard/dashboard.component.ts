cd8e303b21500309d92d5de0dd4e49951e3d58db
Wilson Ortiz <wil82-xbox@hotmail.com>
2025-06-08 00:35:35 -0500

    adicion de mejoras

--- src/app/modulos/dashboard/dashboard.component.ts
+++ src/app/modulos/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: false,
-  
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
+  @ViewChildren('negocioModal, tiendaModal, reportesModal, empleadosModal') 
+  modals!: QueryList<ElementRef>;
 
-  
-  
-
+  ngAfterViewInit() {
+    this.modals.forEach(modal => {
+      const modalElement = modal.nativeElement;
+      modalElement.addEventListener('show.bs.modal', () => {
+        modalElement.removeAttribute('aria-hidden');
+      });
+      modalElement.addEventListener('hidden.bs.modal', () => {
+        modalElement.setAttribute('aria-hidden', 'true');
+      });
+    });
+  }
 }
